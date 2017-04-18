43d3594d152ae31aa5b210cf9f09c2e488b799a3
mogaiskii <sani.mog@gmail.com>
2017-04-18 11:06:53 +0000

    allowed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0(zrru$05+evl7!0c6@dl1nzfn-g*b*eb3ejbparje-d6il8w@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mogaiskii.pythonanywhere.com"]
 
 
 # Application definition
