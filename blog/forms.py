6df10a7a7d2d9ccd6246d0e35eabe9c2cd9170a7
mogaiskii <sani.mog@gmail.com>
2017-09-05 20:34:37 +0700

    Redisign

--- blog/forms.py
+++ blog/forms.py
@@ -6,6 +6,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+    def __init__(self, *args, **kwargs):
+    	super(PostForm,self).__init__(*args,**kwargs)
+    	self.fields['title'].widget=forms.TextInput(attrs={'autocomplite':'off','class':'form-control'})
+    	self.fields['text'].widget=forms.Textarea(attrs={'autocomplite':'off','class':'form-control','rows':'14'})
 
 
 class CommentForm(forms.ModelForm):
