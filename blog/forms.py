3b06752a8b5bc27e60d91c1a438d3e101af4771e
root <root@sani.mog.freeopti.ru>
2018-11-16 12:52:21 +0700

    tabindex

--- blog/forms.py
+++ blog/forms.py
@@ -8,8 +8,8 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text',)
     def __init__(self, *args, **kwargs):
     	super(PostForm,self).__init__(*args,**kwargs)
-    	self.fields['title'].widget=forms.TextInput(attrs={'autocomplite':'off','class':'form-control'})
-    	self.fields['text'].widget=forms.Textarea(attrs={'autocomplite':'off','class':'form-control','rows':'14'})
+    	self.fields['title'].widget=forms.TextInput(attrs={'autocomplite':'off','class':'form-control', 'tabindex': '1'})
+    	self.fields['text'].widget=forms.Textarea(attrs={'autocomplite':'off','class':'form-control','rows':'14', 'tabindex': '2'})
 
 
 class CommentForm(forms.ModelForm):
